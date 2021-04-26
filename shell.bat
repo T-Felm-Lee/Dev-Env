b67ec477ad4d3247699ef4a9bf17c2119f38e6c5
Tanner <62108434+T-Felm-Lee@users.noreply.github.com>
2021-04-26 12:59:26 +0200

    Update shell.bat

--- shell.bat
+++ shell.bat
@@ -1,3 +1,3 @@
 @echo off
 
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+call "\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
