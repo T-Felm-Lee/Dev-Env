2f9aec1c10b527b6cbb2806d3e53f646a0a67f95
Tanner <62108434+T-Felm-Lee@users.noreply.github.com>
2021-06-20 14:44:15 +0200

    added build tools batch files

---
+++ shell_VS2019_BT.bat
@@ -0,0 +1,5 @@
+@echo off
+
+rem shell.bat for VS2019 Community install
+
+call "\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x64
---
+++ shell_VS2019_BT_vscode.bat
@@ -0,0 +1,7 @@
+@echo off
+
+rem shell.bat for VS2019 Community install
+
+call "\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x64
+
+code .
